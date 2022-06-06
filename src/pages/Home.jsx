15bf12c2047bb57b32d32b2b9842a7f47a62a757
unknown <prafultandekar1997@gmail.com>
2022-06-07 00:01:01 +0530

    Evalution3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
+
+
 import React from "react";
+import { Products } from "../components/Products";
 
 const Home = () => {
-  return <div>{/* Code here */}</div>;
+  return (
+    <div
+      style={{
+        maxWidth: "90%",
+        margin: "auto",
+        padding: "6px",
+        // border:"1 px solid yellow"
+        
+      }}
+    >
+      <Products />
+    </div>
+  );
 };
 
 export default Home;
